3476058b3d5e9893f591b21afa3d7ffe6ae76bbc
Caitano <josiasmartinscaitano@gmail.com>
2022-09-12 22:15:32 -0300

    feat: Novo Widget: Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -26,57 +26,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.red, width: 100, height: 100,),
-                Container(color: Colors.blue, width: 50, height: 50,),
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.blue, width: 100, height: 100,),
-                Container(color: Colors.red, width: 50, height: 50,),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(color: Colors.cyan, height: 50, width: 50,),
-                Container(color: Colors.pinkAccent, height: 50, width: 50,),
-                Container(color: Colors.purple, height: 50, width: 50 ,),
-              ],
-            ),
-            Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: Text(
-                'Diamante amarelo',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  print('Voce apertou o botão');
-                },
-                child: Text('Aperte o botão!'))
-
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter first step'),
         ),
-      )
+        body: Container(color: Colors.black,),
+        floatingActionButton: FloatingActionButton(onPressed: (){},)
+      ),
     );
   }
 }
---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,51 @@
+// Container(
+// color: Colors.white,
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(color: Colors.red, width: 100, height: 100,),
+// Container(color: Colors.blue, width: 50, height: 50,),
+// ],
+// ),
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(color: Colors.blue, width: 100, height: 100,),
+// Container(color: Colors.red, width: 50, height: 50,),
+// ],
+// ),
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Container(color: Colors.cyan, height: 50, width: 50,),
+// Container(color: Colors.pinkAccent, height: 50, width: 50,),
+// Container(color: Colors.purple, height: 50, width: 50 ,),
+// ],
+// ),
+// Container(
+// color: Colors.amber,
+// height: 30,
+// width: 300,
+// child: Text(
+// 'Diamante amarelo',
+// style: TextStyle(
+// color: Colors.black,
+// fontSize: 28
+// ),
+// textAlign: TextAlign.center,
+// ),
+// ),
+// ElevatedButton(
+// onPressed: (){
+// print('Voce apertou o botão');
+// },
+// child: Text('Aperte o botão!'))
+//
+// ],
+// ),
+// )
